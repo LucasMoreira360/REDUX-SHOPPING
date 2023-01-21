9a05593a188ed62f47312b009ef7e6fae9152438
Lucas Moreira <lucas_10_moreira@hotmail.com>
2023-01-21 15:43:56 -0300

    feat: add user reducer

---
+++ src/redux/user/reducer.js
@@ -0,0 +1,28 @@
+import UserActionTypes from "./action-types";
+
+const initialState = {
+  currentUser:null,
+};
+
+const useReducer = (state = initialState, action) => {
+  switch(action.type){
+    case UserActionTypes.LOGIN:
+      return {...state, currentUser: action.payload};
+    case UserActionTypes.LOGOUT:
+      return {...state, currentUser:null} 
+    default:
+      return state;
+  }
+
+  
+  // if ( action.type === UserActionTypes.LOGIN){
+  //   return {...state,currentUser: action.payload};
+  // }
+  // if (action.type === UserActionTypes.LOGOUT){
+  //   return { ...state,currentUser: null };
+  // }
+
+  // return state; 
+};
+
+export default useReducer;
